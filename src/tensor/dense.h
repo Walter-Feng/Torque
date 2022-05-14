c72919f073a29b285f578dfbdbfafdba657a27c3
Rui Li <rli3@caltech.edu>
2022-05-13 17:34:30 -0700

    finish writing transpose

--- src/tensor/dense.h
+++ src/tensor/dense.h
@@ -49,17 +49,36 @@ public:
             table_index *= dimension(i);
         }
 
-        this->data = (T *) malloc(sizeof(T) * arma::prod(dimension));
+        this->data = std::unique_ptr<T>((T *) malloc(sizeof(T) * arma::prod(dimension)));
 
         if(source_data) {
-            memcpy(this->data, source_data, sizeof(T) * arma::prod(dimension));
+            memcpy(this->data.get(), source_data, sizeof(T) * arma::prod(dimension));
         } else {
             throw Error("Source data not allocated!");
         }
     }
 
     inline
-    explicit DenseTensor(const DenseTensor & tensor) {
+    explicit DenseTensor(std::unique_ptr<T> && source_data, const arma::uvec & dimension) {
+
+        this->dimension = dimension;
+
+        rank = dimension.n_elem;
+
+        arma::uword table_index = 1;
+
+        this->index_table = arma::uvec(rank);
+
+        for (arma::uword i = 0; i < rank; i++) {
+            this->index_table(i) = table_index;
+            table_index *= dimension(i);
+        }
+
+        this->data = source_data;
+    }
+
+    inline
+    DenseTensor(const DenseTensor & tensor) {
         this->rank = tensor.rank;
         this->dimension = tensor.dimension;
         this->index_table = tensor.index_table;
@@ -84,6 +103,8 @@ public:
     }
 
     /// Modify a number in the tensor
+    /// \param indices indices for each dimension
+    /// \param number the target number (to replace the original one)
     inline
     void modify(const arma::uvec & indices, const T number) {
         this->data.get()[arma::sum(indices % this->index_table)] = number;
@@ -104,18 +125,101 @@ public:
 
     /// Contraction with another tensor
     /// \param tensor another tensor to be contracted with
-    inline
-    DenseTensor<T> contract(const DenseTensor<T> & tensor, const arma::umat & contracting_indices) const {
+    /// \param contracting_indices the corresponding two indices for the dimensions to contract
+    /// from two tensors. It should be a (n x 2) matrix, with first col representing "this" tensor.
+    template<typename U>
+    DenseTensor<std::common_type_t<T, U>>
+    contract(const DenseTensor<U> & tensor, const arma::umat & contracting_indices) const {
+        const arma::uvec this_contracting_indices = contracting_indices.col(0);
+        const arma::uvec that_contracting_indices = contracting_indices.col(1);
+
+        const arma::uvec contract_dimension = this->dimension(this_contracting_indices);
+
+        if(!arma::all(contract_dimension - tensor.dimension(this_contracting_indices) == 0)) {
+            throw Error("The dimensions from two tensors to be contracted do not match");
+        }
+
+
 
 
     }
 
-    /// Transposition of the tensors according to the permutation
+    /// Transposition of the tensors according to the permutation, without changing original data
     /// \param permutation the permutation indices
     inline
-    void transpose(const arma::uvec & permutation) {
+    void soft_transpose(const arma::uvec & permutation) {
+
+        if(permutation.n_elem != rank) {
+            throw Error("The number of permutation does not match the rank of tensor");
+        }
         this->index_table = this->index_table(permutation);
         this->dimension = this->dimension(permutation);
+
+    }
+
+
+    /// Whether this tensor is sorted, i.e. has not been soft transposed.
+    inline
+    bool is_sorted() const {
+        return this->index_table.is_sorted();
+    }
+
+    /// Whether this tensor has stride of the leading dimension equal to 1.
+    inline
+    bool has_leading_dimension() const {
+        return this->index_table(1) == 0;
+    }
+
+    /// Transposition of the tensors according to the permutation, creating new object with new alignment of data.
+    /// This helps keeping the stride of leading dimension equal to 1.
+    /// \param permutation the permutation indices
+    inline
+    DenseTensor<T> hard_transpose(const arma::uvec & permutation) const {
+
+        if(permutation.n_elem != rank) {
+            throw Error("The number of permutation does not match the rank of tensor");
+        }
+
+        const arma::uvec new_dimension = this->dimension(permutation);
+
+        arma::uvec new_table(rank);
+
+        arma::uword table_index = 1;
+
+        for (arma::uword i = 0; i < rank; i++) {
+            new_table(i) = table_index;
+            table_index *= new_dimension(i);
+        }
+
+        const arma::uword total_elem = arma::prod(this->dimension);
+
+        std::unique_ptr<T> new_data((T *) malloc(sizeof(T) * total_elem));
+
+        arma::uvec indices(this->rank);
+
+        const auto data_pointer = this->data.get();
+        const auto new_data_pointer = new_data.get();
+
+        // It is possible that this tensor has been soft transposed, i.e.
+        // the index table may not be in sorted order.
+        const arma::uvec sort_index = arma::sort_index(this->index_table);
+
+        for(arma::uword i=0; i<total_elem; i++) {
+
+            arma::uword temp_i = i;
+
+            for(arma::uword j = this->rank - 1; j > 0; j--) {
+                indices(sort_index(j)) = temp_i / this->index_table(sort_index(j));
+                temp_i = temp_i % this->index_table(sort_index(j));
+            }
+
+            indices(sort_index(0)) = temp_i;
+
+            new_data_pointer[arma::sum(indices(permutation) % new_table)] = data_pointer[i];
+        }
+
+        return DenseTensor<T>(std::move(new_data_pointer), new_dimension);
+
     }
 
 protected:
@@ -123,14 +227,14 @@ protected:
     arma::uword rank;
 
     /// The dimensions at each index of the tensor. The first index is the leading dimension of the
-    /// tensor, i.e. difference of 1 for the first index will result in neighboring address in the
-    /// data.
+    /// tensor with stride equal to 1, i.e. difference of 1 for the first index will result in
+    /// neighboring address in the data.
     arma::uvec dimension;
 
     /// An intermediate table that helps generating the index for the flattened one-dimensional data
     arma::uvec index_table;
 
-    /// Stores data;
+    /// Stores data
     std::unique_ptr<T> data = nullptr;
 };
 
--- test/dense_tensor_test.cpp
+++ test/dense_tensor_test.cpp
@@ -40,6 +40,21 @@ TEST_CASE("dense tensor operation") {
 
         const arma::uvec permutation = {1,0};
 
+        CHECK(tensor.query({1,2}) == 10);
+
+        tensor.soft_transpose(permutation); // Now tensor is 3 x 4 matrix
+
+        CHECK(tensor.query({1, 2}) == 7);
+
+        // tensor_transposed should have original matrix, 4 x 3 matrix
+        const auto tensor_transposed = tensor.hard_transpose(permutation);
+
+        for(arma::uword i=0; i<3; i++) {
+            for(arma::uword j=0; j<4; j++) {
+                CHECK(tensor.query(arma::uvec{i, j}) == tensor_transposed.query(arma::uvec{j, i}));
+            }
+        }
+
     }
 
     SECTION("dense tensor initialization") {
@@ -54,5 +69,23 @@ TEST_CASE("dense tensor operation") {
 
         tensor.initialize(a.data());
         CHECK(tensor.query({0,1,2}) == 11);
+    }
+
+    SECTION("dense tensor transposition") {
+        const int rank = 3; // Matrix
+        const arma::uvec dimension = {2, 2, 3}; // 4x3 matrix
+
+        std::vector<float> a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+        torque::DenseTensor<float> tensor(a.data(), dimension);
+        CHECK(tensor.query({0,0,2}) == 9);
+        CHECK(tensor.query({0,1,2}) == 11);
+        tensor.soft_transpose({0, 2, 1});
+        CHECK(tensor.query({0,2,0}) == 9);
+        CHECK(tensor.query({0,2,1}) == 11);
+
+        const auto tensor_another_transpose = tensor.hard_transpose({1,0,2});
+        CHECK(tensor_another_transpose.query({2, 0, 0}) == 9);
+        CHECK(tensor_another_transpose.query({2, 0, 1}) == 11);
     }
 }
