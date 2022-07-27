422c10fde0e572eb4a3c4c0ff93e69542867b8d9
Rui Li <rli3@caltech.edu>
2022-07-27 15:33:27 -0700

    add debug

--- test/gpu/block_sparse_test.cu
+++ test/gpu/block_sparse_test.cu
@@ -205,6 +205,9 @@ TEST_CASE("block sparse tensor operation") {
     CHECK(contraction_2.query({0, 1}) == 16);
     CHECK(contraction_2.query({1, 1}) == 19);
 
+    tensor_format.begin_points.print("tensor_format.begin_points");
+    tensor_format.end_points.print("tensor_format.end_points");
+    tensor_format.block_n_elem.print("tensor_format.block_n_elem");
     const auto contraction_3 = tensor_format.contract(handle,
                                                       vector_in_tensor,
                                                       arma::umat{{2, 0}});
